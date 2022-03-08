53536c7c8cb171a63c693295fe025bef42cbd829
Felipe dos Santos <ofelipe.dev@gmail.com>
2022-03-08 13:29:39 -0300

    pagination
    implementação do campo de paginação e as arrows de prev e next

--- src/components/Pokedex/index.js
+++ src/components/Pokedex/index.js
@@ -1,5 +1,6 @@
 import PokemonCardList from '../PokemonCardList'
 import './pokedex.css'
+import {FaArrowLeft, FaArrowRight} from 'react-icons/fa'
 
 export default function Pokedex(props){
     const {pokemonsList, loading} = props
@@ -7,11 +8,20 @@ export default function Pokedex(props){
 
     return(
         <main className="pokedex">
-            <h1 className="title">Pokedex</h1>
 
+            <div className='pokedex-top'>
+                <h1 className="pokedex-title">POKEDEX</h1>
+
+                <div className="pagination">
+                    <button className='pag-prev arrows'><FaArrowLeft size={28}/></button>
+                    <button className='pag-next arrows'><FaArrowRight size={28}/></button>
+                    <span className='pag-info'></span>
+                </div>
+            </div>
+            
             {loading ? (
                 <div>CARREGANDO</div> /* trocar para um modal ou tela de loading */
-            ) : (
+                ) : (
                 <div className='pokedex-grid'>
                     {pokemonsList && pokemonsList.map((pokemon, index) => {
                         return(
